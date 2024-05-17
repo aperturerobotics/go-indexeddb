1a266426178e554551a988adda92f135b58ec26f
Christian Stewart <christian@aperture.us>
2024-05-17 00:11:10 -0700

    refactor(durable): rename receiver variables for clarity

--- durable/durable.go
+++ durable/durable.go
@@ -56,8 +56,8 @@ func NewDurableTransaction(db *idb.Database, txnMode idb.TransactionMode, object
 }
 
 // GetObjectStore returns the DurableObjectStore for the given name.
-func (dt *DurableTransaction) GetObjectStore(name string) (*DurableObjectStore, error) {
-	store, ok := dt.objectStores[name]
+func (t *DurableTransaction) GetObjectStore(name string) (*DurableObjectStore, error) {
+	store, ok := t.objectStores[name]
 	if !ok {
 		return nil, errors.New("object store not available in this txn")
 	}
@@ -68,13 +68,13 @@ func (dt *DurableTransaction) GetObjectStore(name string) (*DurableObjectStore, 
 // no-op if the transaction was already committed
 // Returns if the abort request did anything and any error.
 // NOTE: the transaction will commit automatically if the goroutine is backgrounded.
-func (dt *DurableTransaction) Abort() (bool, error) {
-	if dt.txn == nil {
+func (t *DurableTransaction) Abort() (bool, error) {
+	if t.txn == nil {
 		return false, nil
 	}
 
-	err := dt.txn.Abort()
-	dt.txn = nil
+	err := t.txn.Abort()
+	t.txn = nil
 	if err == nil {
 		return true, nil
 	}
@@ -87,13 +87,13 @@ func (dt *DurableTransaction) Abort() (bool, error) {
 // Commit attempts to commit the transaction if one is active.
 // no-op if the transaction was already committed
 // NOTE: the transaction will commit automatically if the goroutine is backgrounded.
-func (dt *DurableTransaction) Commit() error {
-	if dt.txn == nil {
+func (t *DurableTransaction) Commit() error {
+	if t.txn == nil {
 		return nil
 	}
 
-	err := dt.txn.Commit()
-	dt.txn = nil
+	err := t.txn.Commit()
+	t.txn = nil
 	if idb.IsTxnFinishedErr(err) {
 		err = nil
 	}
@@ -101,19 +101,19 @@ func (dt *DurableTransaction) Commit() error {
 }
 
 // ensureTransaction ensures dt.txn is not nil.
-func (dt *DurableTransaction) ensureTransaction() error {
-	if dt.txn != nil {
+func (t *DurableTransaction) ensureTransaction() error {
+	if t.txn != nil {
 		return nil
 	}
 
-	txn, err := dt.db.Transaction(dt.txnMode, dt.objectStoreNames[0], dt.objectStoreNames[1:]...)
+	txn, err := t.db.Transaction(t.txnMode, t.objectStoreNames[0], t.objectStoreNames[1:]...)
 	if err != nil {
 		return err
 	}
-	dt.txn = txn
+	t.txn = txn
 
-	for name, durableStore := range dt.objectStores {
-		store, err := dt.txn.ObjectStore(name)
+	for name, durableStore := range t.objectStores {
+		store, err := t.txn.ObjectStore(name)
 		if err != nil {
 			return err
 		}
@@ -124,13 +124,13 @@ func (dt *DurableTransaction) ensureTransaction() error {
 }
 
 // runWithRetry retries if we get a Transaction Finished error.
-func (dt *DurableTransaction) runWithRetry(fn func(txn *idb.Transaction) error) error {
+func (t *DurableTransaction) runWithRetry(fn func(txn *idb.Transaction) error) error {
 	for {
-		if err := dt.ensureTransaction(); err != nil {
+		if err := t.ensureTransaction(); err != nil {
 			return err
 		}
 
-		err := fn(dt.txn)
+		err := fn(t.txn)
 		if err == nil {
 			return nil
 		}
@@ -140,7 +140,7 @@ func (dt *DurableTransaction) runWithRetry(fn func(txn *idb.Transaction) error) 
 		}
 
 		// mark txn as nil and retry
-		dt.txn = nil
+		t.txn = nil
 	}
 }
 
@@ -152,9 +152,9 @@ type DurableObjectStore struct {
 }
 
 // Add creates a structured clone of the value, and stores the cloned value in the object store. This is for adding new records to an object store.
-func (dos *DurableObjectStore) Add(ctx context.Context, value safejs.Value) error {
-	return dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.Add(value)
+func (d *DurableObjectStore) Add(ctx context.Context, value safejs.Value) error {
+	return d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.Add(value)
 		if err != nil {
 			return err
 		}
@@ -163,9 +163,9 @@ func (dos *DurableObjectStore) Add(ctx context.Context, value safejs.Value) erro
 }
 
 // Clear clears the entire object store. This is for deleting all current records out of an object store.
-func (dos *DurableObjectStore) Clear(ctx context.Context) error {
-	return dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.Clear()
+func (d *DurableObjectStore) Clear(ctx context.Context) error {
+	return d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.Clear()
 		if err != nil {
 			return err
 		}
@@ -174,10 +174,10 @@ func (dos *DurableObjectStore) Clear(ctx context.Context) error {
 }
 
 // Count returns the total number of records in the store.
-func (dos *DurableObjectStore) Count(ctx context.Context) (uint, error) {
+func (d *DurableObjectStore) Count(ctx context.Context) (uint, error) {
 	var cnt uint
-	rerr := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.Count()
+	rerr := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.Count()
 		if err != nil {
 			return err
 		}
@@ -192,9 +192,9 @@ func (dos *DurableObjectStore) Count(ctx context.Context) (uint, error) {
 }
 
 // Delete deletes the store object selected by the specified key. This is for deleting individual records out of an object store.
-func (dos *DurableObjectStore) Delete(ctx context.Context, key safejs.Value) error {
-	return dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.Delete(key)
+func (d *DurableObjectStore) Delete(ctx context.Context, key safejs.Value) error {
+	return d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.Delete(key)
 		if err != nil {
 			return err
 		}
@@ -203,10 +203,10 @@ func (dos *DurableObjectStore) Delete(ctx context.Context, key safejs.Value) err
 }
 
 // Get returns the objects selected by the specified key. This is for retrieving specific records from an object store.
-func (dos *DurableObjectStore) Get(ctx context.Context, key safejs.Value) (safejs.Value, error) {
+func (d *DurableObjectStore) Get(ctx context.Context, key safejs.Value) (safejs.Value, error) {
 	var value safejs.Value
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.Get(key)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.Get(key)
 		if err != nil {
 			return err
 		}
@@ -221,9 +221,9 @@ func (dos *DurableObjectStore) Get(ctx context.Context, key safejs.Value) (safej
 }
 
 // Put creates a structured clone of the value, and stores the cloned value in the object store. This is for updating existing records in an object store when the transaction's mode is readwrite.
-func (dos *DurableObjectStore) Put(ctx context.Context, value safejs.Value) error {
-	return dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.Put(value)
+func (d *DurableObjectStore) Put(ctx context.Context, value safejs.Value) error {
+	return d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.Put(value)
 		if err != nil {
 			return err
 		}
@@ -233,9 +233,9 @@ func (dos *DurableObjectStore) Put(ctx context.Context, value safejs.Value) erro
 }
 
 // PutKey is the same as Put, but includes the key to use to identify the record.
-func (dos *DurableObjectStore) PutKey(ctx context.Context, key, value safejs.Value) error {
-	return dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.PutKey(key, value)
+func (d *DurableObjectStore) PutKey(ctx context.Context, key, value safejs.Value) error {
+	return d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.PutKey(key, value)
 		if err != nil {
 			return err
 		}
@@ -245,9 +245,9 @@ func (dos *DurableObjectStore) PutKey(ctx context.Context, key, value safejs.Val
 }
 
 // AddKey is the same as Add, but includes the key to use to identify the record.
-func (dos *DurableObjectStore) AddKey(ctx context.Context, key, value safejs.Value) error {
-	return dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.AddKey(key, value)
+func (d *DurableObjectStore) AddKey(ctx context.Context, key, value safejs.Value) error {
+	return d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.AddKey(key, value)
 		if err != nil {
 			return err
 		}
@@ -256,10 +256,10 @@ func (dos *DurableObjectStore) AddKey(ctx context.Context, key, value safejs.Val
 }
 
 // GetKey retrieves and returns the record key for the object matching the specified parameter.
-func (dos *DurableObjectStore) GetKey(ctx context.Context, value safejs.Value) (safejs.Value, error) {
+func (d *DurableObjectStore) GetKey(ctx context.Context, value safejs.Value) (safejs.Value, error) {
 	var key safejs.Value
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.GetKey(value)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.GetKey(value)
 		if err != nil {
 			return err
 		}
@@ -274,10 +274,10 @@ func (dos *DurableObjectStore) GetKey(ctx context.Context, value safejs.Value) (
 }
 
 // CountKey returns a UintRequest, and, in a separate thread, returns the total number of records that match the provided key.
-func (dos *DurableObjectStore) CountKey(ctx context.Context, key safejs.Value) (uint, error) {
+func (d *DurableObjectStore) CountKey(ctx context.Context, key safejs.Value) (uint, error) {
 	var cnt uint
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.CountKey(key)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.CountKey(key)
 		if err != nil {
 			return err
 		}
@@ -292,10 +292,10 @@ func (dos *DurableObjectStore) CountKey(ctx context.Context, key safejs.Value) (
 }
 
 // CountRange returns a UintRequest, and, in a separate thread, returns the total number of records that match the provided KeyRange.
-func (dos *DurableObjectStore) CountRange(ctx context.Context, keyRange *idb.KeyRange) (uint, error) {
+func (d *DurableObjectStore) CountRange(ctx context.Context, keyRange *idb.KeyRange) (uint, error) {
 	var cnt uint
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.CountRange(keyRange)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.CountRange(keyRange)
 		if err != nil {
 			return err
 		}
@@ -310,10 +310,10 @@ func (dos *DurableObjectStore) CountRange(ctx context.Context, keyRange *idb.Key
 }
 
 // GetAllKeys returns an ArrayRequest that retrieves record keys for all objects in the object store.
-func (dos *DurableObjectStore) GetAllKeys(ctx context.Context) ([]safejs.Value, error) {
+func (d *DurableObjectStore) GetAllKeys(ctx context.Context) ([]safejs.Value, error) {
 	var keys []safejs.Value
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.GetAllKeys()
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.GetAllKeys()
 		if err != nil {
 			return err
 		}
@@ -328,10 +328,10 @@ func (dos *DurableObjectStore) GetAllKeys(ctx context.Context) ([]safejs.Value, 
 }
 
 // GetAllKeysRange returns an ArrayRequest that retrieves record keys for all objects in the object store matching the specified query. If maxCount is 0, retrieves all objects matching the query.
-func (dos *DurableObjectStore) GetAllKeysRange(ctx context.Context, query *idb.KeyRange, maxCount uint) ([]safejs.Value, error) {
+func (d *DurableObjectStore) GetAllKeysRange(ctx context.Context, query *idb.KeyRange, maxCount uint) ([]safejs.Value, error) {
 	var keys []safejs.Value
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.GetAllKeysRange(query, maxCount)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.GetAllKeysRange(query, maxCount)
 		if err != nil {
 			return err
 		}
@@ -346,10 +346,10 @@ func (dos *DurableObjectStore) GetAllKeysRange(ctx context.Context, query *idb.K
 }
 
 // OpenCursor returns a CursorWithValueRequest, and, in a separate thread, returns a new CursorWithValue. Used for iterating through an object store by primary key with a cursor.
-func (dos *DurableObjectStore) OpenCursor(ctx context.Context, direction idb.CursorDirection) (*idb.CursorWithValue, error) {
+func (d *DurableObjectStore) OpenCursor(ctx context.Context, direction idb.CursorDirection) (*idb.CursorWithValue, error) {
 	var cursor *idb.CursorWithValue
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.OpenCursor(direction)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.OpenCursor(direction)
 		if err != nil {
 			return err
 		}
@@ -364,10 +364,10 @@ func (dos *DurableObjectStore) OpenCursor(ctx context.Context, direction idb.Cur
 }
 
 // OpenCursorKey is the same as OpenCursor, but opens a cursor over the given key instead.
-func (dos *DurableObjectStore) OpenCursorKey(ctx context.Context, key safejs.Value, direction idb.CursorDirection) (*idb.CursorWithValue, error) {
+func (d *DurableObjectStore) OpenCursorKey(ctx context.Context, key safejs.Value, direction idb.CursorDirection) (*idb.CursorWithValue, error) {
 	var cursor *idb.CursorWithValue
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.OpenCursorKey(key, direction)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.OpenCursorKey(key, direction)
 		if err != nil {
 			return err
 		}
@@ -382,10 +382,10 @@ func (dos *DurableObjectStore) OpenCursorKey(ctx context.Context, key safejs.Val
 }
 
 // OpenCursorRange is the same as OpenCursor, but opens a cursor over the given range instead.
-func (dos *DurableObjectStore) OpenCursorRange(ctx context.Context, keyRange *idb.KeyRange, direction idb.CursorDirection) (*idb.CursorWithValue, error) {
+func (d *DurableObjectStore) OpenCursorRange(ctx context.Context, keyRange *idb.KeyRange, direction idb.CursorDirection) (*idb.CursorWithValue, error) {
 	var cursor *idb.CursorWithValue
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.OpenCursorRange(keyRange, direction)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.OpenCursorRange(keyRange, direction)
 		if err != nil {
 			return err
 		}
@@ -400,10 +400,10 @@ func (dos *DurableObjectStore) OpenCursorRange(ctx context.Context, keyRange *id
 }
 
 // OpenKeyCursor returns a CursorRequest, and, in a separate thread, returns a new Cursor. Used for iterating through all keys in an object store.
-func (dos *DurableObjectStore) OpenKeyCursor(ctx context.Context, direction idb.CursorDirection) (*idb.Cursor, error) {
+func (d *DurableObjectStore) OpenKeyCursor(ctx context.Context, direction idb.CursorDirection) (*idb.Cursor, error) {
 	var cursor *idb.Cursor
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.OpenKeyCursor(direction)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.OpenKeyCursor(direction)
 		if err != nil {
 			return err
 		}
@@ -418,10 +418,10 @@ func (dos *DurableObjectStore) OpenKeyCursor(ctx context.Context, direction idb.
 }
 
 // OpenKeyCursorKey is the same as OpenKeyCursor, but opens a cursor over the given key instead.
-func (dos *DurableObjectStore) OpenKeyCursorKey(ctx context.Context, key safejs.Value, direction idb.CursorDirection) (*idb.Cursor, error) {
+func (d *DurableObjectStore) OpenKeyCursorKey(ctx context.Context, key safejs.Value, direction idb.CursorDirection) (*idb.Cursor, error) {
 	var cursor *idb.Cursor
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.OpenKeyCursorKey(key, direction)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.OpenKeyCursorKey(key, direction)
 		if err != nil {
 			return err
 		}
@@ -436,10 +436,10 @@ func (dos *DurableObjectStore) OpenKeyCursorKey(ctx context.Context, key safejs.
 }
 
 // OpenKeyCursorRange is the same as OpenKeyCursor, but opens a cursor over the given key range instead.
-func (dos *DurableObjectStore) OpenKeyCursorRange(ctx context.Context, keyRange *idb.KeyRange, direction idb.CursorDirection) (*idb.Cursor, error) {
+func (d *DurableObjectStore) OpenKeyCursorRange(ctx context.Context, keyRange *idb.KeyRange, direction idb.CursorDirection) (*idb.Cursor, error) {
 	var cursor *idb.Cursor
-	err := dos.dt.runWithRetry(func(txn *idb.Transaction) error {
-		req, err := dos.store.OpenKeyCursorRange(keyRange, direction)
+	err := d.dt.runWithRetry(func(txn *idb.Transaction) error {
+		req, err := d.store.OpenKeyCursorRange(keyRange, direction)
 		if err != nil {
 			return err
 		}
